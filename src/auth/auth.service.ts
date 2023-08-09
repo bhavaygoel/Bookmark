50c1f6015074078ac97a411152e01a78b212c127
bhavaygoel <goelbhavay@gmail.com>
2023-08-09 19:30:48 +0530

    singup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,6 +34,30 @@ export class AuthService {
             throw error;
         }
     }
-    signin(){}
+    async signin(dto: AuthDto){
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        });
+        if(!user)
+            throw new ForbiddenException(
+        'Credentials incorrect',
+        );
+
+
+        const pwMatches = await argon.verify(
+            user.hash,
+            dto.password,
+        )
+
+        if(!pwMatches)
+            throw new ForbiddenException(
+        'Credentials incorrect',
+        );
+
+        delete user.hash;
+        return user;
+    }
 
 }
